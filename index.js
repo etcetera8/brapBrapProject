ac2d7c70aa3668fcb60310cd11cde32dfaed930f
Parker Lindley <fugazi8@gmail.com>
2017-09-14 09:12:34 -0600

    add geolocation - save lat long to vars

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
-function test(){
-  console.log("click working")
+//ONLOAD PREPARE
+addEventListener("load", clickHandleSetUp);
+addEventListener("load", latLong);
+
+//GEOLOCATION
+if ("geolocation" in navigator) {
+  console.log("geolocation attained")
+} else {
+  console.log("geolocation not attained")
 }
 
-addEventListener("load", clickHandleSetUp);
+function latLong () {
+  if (!navigator.geolocation) {
+    alert("Geolocation not supported, some features may not work");
+    return;
+  }
 
+function success(position) {
+  var lat = position.coords.latitude;
+  var long = position.coords.longitude;
+  console.log(lat);
+  console.log(long);
+}
+
+function error() {
+  console.log("geolocation error");
+}
+
+navigator.geolocation.getCurrentPosition(success, error);
+
+}
+
+
+//CLICK HANDLER SETUP
 function clickHandleSetUp() {
   var key = '200021803-8942a6c5fbed31eeee08f074613141b1';
   var url = 'https://www.mtbproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=200021803-8942a6c5fbed31eeee08f074613141b1';
@@ -11,6 +39,7 @@ function clickHandleSetUp() {
   button.addEventListener("click", clickHandler);
 };
 
+// CLICK HANDLER EVENT / THE CLICK
 function clickHandler(event) {
   var request = new XMLHttpRequest();
   var url = 'https://www.mtbproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=200021803-8942a6c5fbed31eeee08f074613141b1';
@@ -23,6 +52,7 @@ function clickHandler(event) {
   };
 }
 
+// STORE THE DATA FROM THE CLICK
 function responseHandler() {
   if (this.status >= 200 && this.status < 400) {
     //Success!
