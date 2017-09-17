6bb10b136efcb8b1bcdf2946d6e74221fe73471f
Parker Lindley <fugazi8@gmail.com>
2017-09-16 18:00:00 -0600

    add api image call and redone screens for api call area

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//GLOBAL VARIABLES
+//global variables
 var lat = 0;
 var long = 0;
 var key = 0;
@@ -17,23 +17,18 @@ function latLong () {
 function success(position) {
   lat = Math.round(position.coords.latitude);
   long = Math.round(position.coords.longitude);
-  console.log(lat);
-  console.log(long);
 }
 
 function error() {
   console.log("geolocation error");
 }
-
-navigator.geolocation.getCurrentPosition(success, error);
-
+  navigator.geolocation.getCurrentPosition(success, error);
 }
 
 
 //CLICK HANDLER SETUP
 function clickHandleSetUp() {
   key = '200021803-8942a6c5fbed31eeee08f074613141b1';
-  //var url = 'https://www.mtbproject.com/data/get-trails?lat='+ lat +'&lon='+ long +'&maxDistance=9&key=200021803-8942a6c5fbed31eeee08f074613141b1';
   var button= document.getElementById("magic");
   button.addEventListener("click", clickHandler);
 };
@@ -53,11 +48,22 @@ function clickHandler(event) {
 
 // STORE THE DATA FROM THE CLICK
 function responseHandler() {
+
   if (this.status >= 200 && this.status < 400) {
     //Success!
     var data = JSON.parse(this.responseText);
-    console.log(data)
-  } else {
+
+
+      var photo = document.createElement("img");
+      photo.src = data.trails[0].imgSmallMed;
+      photo.id = "picture"
+      var tag = document.getElementById("trailFindLeft");
+      tag.appendChild(photo);
+      return;
+
+  }
+
+  else {
     console.log('server reached, returns error')
   }
 }
